8b84284b85224c1de146dcc244e7f9f379eff926
RANA ARIFIN <108522512+085871373603@users.noreply.github.com>
2026-09-25 09:04:34 +0700

    Add checkoutWhatsapp field to configuration

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+
 window.CFG = {
   owner: "085871373603",   // ganti: username/organisasi GitHub
   repo: "Katalog",          // ganti: nama repo katalog ini
@@ -5,5 +6,5 @@ window.CFG = {
   dataPath: "products.json",
   imageDir: "uploads",
   storeName: "Katalog Saya",
-  whatsapp: ""                // opsional, contoh: 6281234567890
+  checkoutWhatsapp: "085871373603"   // nomor WA tujuan checkout
 };
